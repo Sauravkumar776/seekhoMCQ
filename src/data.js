3822caa2d0b907f29bdde94e4e08b448c3b17539
sauravkumar776 <ashukumar752861@gmail.com>
2023-03-24 15:40:30 +0530

    Adds questions in data.js file.

---
+++ src/data.js
@@ -0,0 +1,35 @@
+const data = [
+  {
+    id: "1",
+    question: "What is the smallest unit of data?",
+    options: ["Gigabyte", "Bit", "Byte", "Terabyte"],
+    answer: "Bit",
+  },
+  {
+    id: "2",
+    question:
+    "Which veda depicts the information about the most ancient vedic age culture",
+    options: ["Atharvaveda", "Samaveda", "Rig Veda", "Yajurveda"],
+    answer: "Rig Veda",
+  },
+  {
+    id: "3",
+    question: "The Grand Canyon located in which country?",
+    options: ["Ghana", "US", "Canada", "Russia"],
+    answer: "US",
+  },
+  {
+    id: "4",
+    question: "Which planet is known as the Red Planet?",
+    options: ["Venus", "Mars", "Jupiter", "Saturn"],
+    answer: "Mars",
+  },
+  {
+    id: "5",
+    question: "A line which cuts a pair of parallel lines is called",
+    options: ["Tangent", "Chord", "Traversal", "Intersector"],
+    answer: "Traversal",
+  },
+];
+
+export default data;
