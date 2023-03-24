ad6677f99299cd3ec1ac62a787dd605f1173e6da
sauravkumar776 <ashukumar752861@gmail.com>
2023-03-24 19:27:56 +0530

    adds show-score component

---
+++ src/components/showScore/show-score.component.jsx
@@ -0,0 +1,36 @@
+import { useContext } from "react"
+import { QuestionContext } from "../../context/QuestionContext";
+import styles from './show-score.module.css';
+
+const ShowScore = () => {
+
+    const {
+        questions,
+        setCurrentQuestion,
+        setSelectedAnswers,
+        score,
+        setScore,
+        setShowScore,
+        setSecondsLeft
+      } = useContext(QuestionContext);
+
+      
+    const handleRestart = () => {
+        setCurrentQuestion(0);
+        setShowScore(false);
+        setScore(0);
+        setSecondsLeft(60);
+        setSelectedAnswers({});
+      };
+
+    return(
+        <div className={styles.scoreContainer}>
+        <h2>
+          You scored {score} out of {questions.length}!
+        </h2>
+        <button onClick={handleRestart}>Restart</button>
+      </div>
+    )
+}
+
+export default ShowScore
