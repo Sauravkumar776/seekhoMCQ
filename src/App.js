8aef22d13fad588566f59a6032a7756605957fb0
sauravkumar776 <ashukumar752861@gmail.com>
2023-03-25 05:45:11 +0530

    Wraps the asssessment app with ScoreProvider.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import AssessmentApp from "./components/assessmentPage/assessment-page.component";
 import { QuestionProvider } from "./context/QuestionContext";
+import { ScoreProvider } from "./context/ScoreContext";
 
 function App() {
   return (
     <QuestionProvider>
+    <ScoreProvider>
       <AssessmentApp />
+      </ScoreProvider>
     </QuestionProvider>
   );
 }
